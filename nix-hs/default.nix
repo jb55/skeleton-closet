859e482d62188803189a5bd271e8c0f837b98a70
William Casarin <bill@casarin.me>
2015-07-23 10:48:32 -0700

    use basename for nix-hs instead of prompt
    Eventually do `{{ name ? $basename }}` once it's supported

--- nix-hs/default.nix
+++ nix-hs/default.nix
@@ -1,2 +1,2 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc7101" }:
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./{{ name }}.nix { }
+nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./{{ $basename }}.nix { }
